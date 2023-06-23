5a7b7780f19ba941ad827cd86f0fe4f57951ef0a
TimVeenboer <timveenboer_19@hotmail.com>
2023-06-23 12:14:38 +0200

    idcg fix, S sorteren

--- src/optimisation.py
+++ src/optimisation.py
@@ -207,6 +207,8 @@ def fairness_optimisation_dcg_change(
         f"Runing fairness optimisation on '{fairness_mode}', {uepsilon}, {iepsilon}")
     print(f"Active users: {U[:, 0].sum()}, Inactive users: {U[:, 1].sum()}")
 
+    S = -np.sort(-S)
+
     # V1: No. of users
     # V2: No. of top items (topk)
     # V3: No. of user groups
@@ -237,13 +239,13 @@ def fairness_optimisation_dcg_change(
     elif fairness_mode == 'C':
         ### C-Fairness: NDCG_Best: group_ndcg_v[1] - group_ndcg_v[0] ###
         model.objective = maximize(xsum(
-            (S[i][j] * W[i][j]) for i in V1 for j in V2) - uepsilon * (group_ndcg_v[0] - group_ndcg_v[1]))
+            (S[i][j] * W[i][j]) for i in V1 for j in V2) - uepsilon * (group_ndcg_v[1] - group_ndcg_v[0]))
     elif fairness_mode == 'P':
         model.objective = maximize(xsum(
             (S[i][j] * W[i][j]) for i in V1 for j in V2) - iepsilon * (item_group[0] - item_group[1]))
     elif fairness_mode == 'CP':
         model.objective = maximize(xsum(
-            (S[i][j] * W[i][j]) for i in V1 for j in V2) - uepsilon * (group_ndcg_v[0] - group_ndcg_v[1]) 
+            (S[i][j] * W[i][j]) for i in V1 for j in V2) - uepsilon * (group_ndcg_v[1] - group_ndcg_v[0]) 
              - iepsilon * (item_group[0] - item_group[1]))
 
     # first constraint: the number of 1 in W should be equal to top-k, recommending top-k best items
@@ -252,7 +254,7 @@ def fairness_optimisation_dcg_change(
         model += xsum(W[i][j] for j in V2) == k
 
     for i in V1:
-        user_idcg_i = 4.254494511770458
+        user_idcg_i = 4.543559338088346
 
         model += user_dcg[i] == xsum((W[i][j] * Ahelp[i][j])/np.log2(j+2) for j in V2)
         model += user_ndcg[i] == user_dcg[i] / user_idcg_i
@@ -263,7 +265,7 @@ def fairness_optimisation_dcg_change(
                                         for j in V2) / len(train_checkins[i])
 
     for k in V3:
-        model += group_ndcg_v[k] == xsum(user_ndcg[i] * U[i][k] for i in V1) / U[:, k].sum()
+        model += group_ndcg_v[k] == xsum(user_ndcg[i] * U[i][k] for i in V1)
         model += group_precision[k] == xsum(user_precision[i]
                                             * U[i][k] for i in V1)
         model += group_recall[k] == xsum(user_recall[i] * U[i][k] for i in V1)
